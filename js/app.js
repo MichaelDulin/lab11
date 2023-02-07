e76bbfeac296a49a2b9c4005561716076b3303ec
MichaelDulin <michaeldulin1211@gmail.com>
2023-02-06 18:34:12 -0800

    styled and finalized

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
-let container = document.querySelector('section');
-let button = document.querySelector('section + button');
+let itemContainer = document.querySelector('section');
+let resultButton = document.querySelector('section + div');
 
 let img1 = document.querySelector('section img:first-child');
 let img2 = document.querySelector('section img:nth-child(2)');
@@ -10,47 +10,23 @@ let img3 = document.querySelector('section img:nth-child(3)');
 let maxChoices = 25;
 let curChoices = 0;
 
-let allItemsArr = [];
+const state = {
+  allItemsArr: [],
+};
 
-function Item(name, fileExtension = 'jpg'){
+function Item(name, src){
   this.name = name;
-  this.src = `img/${name}.${fileExtension}`;
+  this.src = src;
   this.views = 0;
   this.likes = 0;
 }
-
-let bag = new Item('bag');
-let banana = new Item('banana');
-let bathroom = new Item('bathroom');
-let boots = new Item('boots');
-let breakfast = new Item('breakfast');
-let bubblegum = new Item('bubblegum');
-let chair = new Item('chair');
-let cthulhu = new Item('cthulhu');
-let dogDuck = new Item('dog-duck');
-let dragon = new Item('dragon');
-let pen = new Item('pen');
-let petSweep = new Item('pet-sweep');
-let scissors = new Item('scissors');
-let shark = new Item('shark');
-let sweep = new Item('sweep', 'png');
-let tauntaun = new Item('tauntaun');
-let unicorn = new Item('unicorn');
-let waterCan = new Item('water-can');
-let wineGlass = new Item('wine-glass');
-
-
-allItemsArr = [
-  bag, banana, bathroom, boots, breakfast, bubblegum, chair, 
-  cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark,
-  sweep, tauntaun, unicorn, waterCan, wineGlass];
 
 function renderItems() {
   let choice1 = randomItem();
   let choice2 = randomItem();
   let choice3 = randomItem();
   function randomItem () {
-    return Math.floor(Math.random() * allItemsArr.length);
+    return Math.floor(Math.random() * state.allItemsArr.length);
   }
   while (choice1 === choice2 || choice2 === choice3 || choice3 === choice1) {
     if (choice1 === choice2 || choice1 === choice3){
@@ -59,42 +35,69 @@ function renderItems() {
       choice2 = randomItem();
     }
   }
-  choice1.src = allItemsArr[choice1].src;
-  choice2.src = allItemsArr[choice2].src;
-  choice3.src = allItemsArr[choice3].src;
-  choice1.alt = allItemsArr[choice1].name;
-  choice2.alt = allItemsArr[choice2].name;
-  choice3.alt = allItemsArr[choice3].name;
-  allItemsArr[choice1].views++;
-  allItemsArr[choice2].views++;
-  allItemsArr[choice3].views++;
-  curChoices++;
+  img1.src = state.allItemsArr[choice1].src;
+  img2.src = state.allItemsArr[choice2].src;
+  img3.src = state.allItemsArr[choice3].src;
+  img1.alt = state.allItemsArr[choice1].name;
+  img2.alt = state.allItemsArr[choice2].name;
+  img3.alt = state.allItemsArr[choice3].name;
+  state.allItemsArr[choice1].views++;
+  state.allItemsArr[choice2].views++;
+  state.allItemsArr[choice3].views++;
 }
 
 function renderResults() {
   let ul = document.querySelector('ul');
-  for (let i = 0; i < allItemsArrlength; i++) {
+  for (let i = 0; i < state.allItemsArr.length; i++) {
     let li = document.createElement('li');
-    li.textContent = `${allItemsArr[i].name}: ${allItemsArr[i].views} views and ${allItemsArr[i].likes} likes`;
+    li.textContent = `${state.allItemsArr[i].name}: ${state.allItemsArr[i].views} views and ${state.allItemsArr[i].likes} likes`;
     ul.appendChild(li);
   }
 }
 
 function handleItemClick(event) {
+  if (event.target === itemContainer){
+    alert('Try clicking an image!');
+  }
+  curChoices++;
   let likedItem = event.target.alt;
-  for (let i = 0; i < allItemsArr.length; i++) {
-    if (allItemsArr[i].name === likedItem) {
-      allItemsArr[i].likes++;
+  for (let i = 0; i < state.allItemsArr.length; i++) {
+    if (state.allItemsArr[i].name === likedItem) {
+      state.allItemsArr[i].likes++;
     }
   }
   if (curChoices < maxChoices) {
     renderItems();
   } else {
-    container.removeEventListener('click', handleItemClick);
-    button.removeEventListener('click', renderResults);
+    itemContainer.removeEventListener('click', handleItemClick);
+    resultButton.addEventListener('click', renderResults);
+    resultButton.className = 'clicks-allowed';
+    itemContainer.className = 'no-voting';
   }
 }
 
+let bag = new Item('Bag', './img/bag.jpg');
+let banana = new Item('Banana', './img/banana.jpg');
+let bathroom = new Item('Bathroom', './img/bathroom.jpg');
+let boots = new Item('Boots', './img/boots.jpg');
+let breakfast = new Item('Breakfast', './img/breakfast.jpg');
+let bubblegum = new Item('Bubblegum', './img/bubblegum.jpg');
+let chair = new Item('Chair', './img/chair.jpg');
+let cthulhu = new Item('Cthulhu', './img/cthulhu.jpg');
+let dogDuck = new Item('Dog Duck', './img/dog-duck.jpg');
+let dragon = new Item('Dragon', './img/dragon.jpg');
+let pen = new Item('Pen', './img/pen.jpg');
+let petSweep = new Item('Pet Sweep', './img/pet-sweep.jpg');
+let scissors = new Item('Scissors', './img/scissors.jpg');
+let shark = new Item('Shark', './img/shark.jpg');
+let sweep = new Item('Sweep', './img/sweep.png');
+let tauntaun = new Item('Tauntaun', './img/tauntaun.jpg');
+let unicorn = new Item('Unicorn', './img/unicorn.jpg');
+let waterCan = new Item('Water Can', './img/water-can.jpg');
+let wineGlass = new Item('Wine Glass', './img/wine-glass.jpg');
+state.allItemsArr.push (bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark,sweep, tauntaun, unicorn, waterCan, wineGlass);
+
+
 renderItems();
 
-container.addEventListener('click', handleItemClick);
+itemContainer.addEventListener('click', handleItemClick);
